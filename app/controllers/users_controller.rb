b6da0674eab4f94b1631ad987e971dc9b71fa5de
Gioanna Rheumer <ratofkryll@gmail.com>
2021-09-29 14:28:26 -0700

    update user routes for simplicity, add ability to create account, redirect new signups to truck list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,13 +6,19 @@ class UsersController < ApplicationController
 
   # POST /users
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
 
     if @user.save
       session[:user_id] = @user.id
-      redirect_to @user, notice: 'User was successfully created.'
+      redirect_to trucks_path, notice: 'User was successfully created.'
     else
       render action: "new"
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :company_id, :phone_number, :role, )
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,19 @@
 Rails.application.routes.draw do
+  root :to => "welcome#index"
+
+  resources :users, only: [:new, :create]
+
   resources :trucks do
     member do
       put :reserve
     end
   end
-  root :to => "welcome#index"
 
   get 'welcome/index'
   get 'sessions/new'
   get 'register' => 'trucks#new', :as => :register
   get 'companies/new'
   get 'companies/create'
-  get 'users/new'
-  post 'users/create'
   post '/login' => 'sessions#new', :as => :login
   post '/logout' => 'sessions#destroy', :as => :logout
   post '/sessions/create' => 'sessions#create'
